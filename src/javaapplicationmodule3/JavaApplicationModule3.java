0236b2fa80d98df11b3ff6d1bd95a65dc61091e3
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-03-20 22:06:23 -0500

    Java Module Prac

---
+++ src/javaapplicationmodule3/JavaApplicationModule3.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationmodule3;
+
+/**
+ *
+ * @author Simbatronix
+ */
+public class JavaApplicationModule3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println(5 % 3);
+      System.out.println(5 % 1);
+      System.out.println(5==3);
+      System.out.println(5 !=3);
+      System.out.println(5>3);
+      System.out.println(5<3);
+      System.out.println(5 <= 5);
+      System.out.println(5 >= 5);
+      System.out.println(5 & 3);
+      System.out.println(5 ^ 3);
+      System.out.println(5 | 3);
+      System.out.println(~5);
+      System.out.println (~(-5));
+      System.out.println (~(-5));
+      System.out.println((5>3)&&(6>3));
+      System.out.println((5>3)&&(6<3));
+      System.out.println((5>3)||(6>3));
+      System.out.println((5>3)||(6<3));
+      System.out.println (!(5>3));
+      System.out.println (5/2);
+    }
+    
+    
+}
